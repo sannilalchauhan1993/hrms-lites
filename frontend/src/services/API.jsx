237edc766342711c7ad23d88afeb8e56d1974d01
Your Name <you@example.com>
2026-02-27 16:30:35 +0530

    added

--- frontend/src/services/API.jsx
+++ frontend/src/services/API.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_URL?.replace(/\/$/, "") ||
-  "http://127.0.0.1:8000";
+  "https://hrm-lites.onrender.com/";
 
 /* Axios instance */
 const api = axios.create({
